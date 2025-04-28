62982823413397db944dc61eced98d43bc6b7070
创创刘 <2514573000@qq.com>
2025-04-28 14:04:03 +0800

    add  前端项目初始化

--- yuzi-generator-web-backend/src/main/java/com/liucc/web/model/entity/Generator.java
+++ yuzi-generator-web-backend/src/main/java/com/liucc/web/model/entity/Generator.java
@@ -1,9 +1,6 @@
 package com.liucc.web.model.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -95,6 +92,7 @@ public class Generator implements Serializable {
     /**
      * 是否删除
      */
+    @TableLogic
     private Integer isDelete;
 
     @TableField(exist = false)
